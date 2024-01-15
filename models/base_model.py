618858bd0fe1e64fb9b47084b4ad85603292b455
otienojoseph <otienoj.cruz@gmail.com>
2024-01-15 17:44:25 +0300

    fix: Add id as string

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
                     setattr(self, key, value)
 
         else:
-            self.id = uuid4()
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
