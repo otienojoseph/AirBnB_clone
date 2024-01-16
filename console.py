41f39d69a1091790d2ce2b9e01b96eee83b380ee
Travis <ondegotravis@gmail.com>
2024-01-16 23:24:03 +0300

    Initial console entry point

--- console.py
+++ console.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 """This module contains console functions"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Program that contains entry point for the command interpreter"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Command to quit/exit the program"""
+        return True
+
+    do_EOF = do_quit
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
